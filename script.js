e54dee309d09b7704f99523a27a9d55d7467e04f
Ny Hatsune <67290899+NyKu-Hatsune@users.noreply.github.com>
2024-04-12 23:30:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var button = document.querySelector('.button');
+    var playButton = document.getElementById('playButton');
+    var video = document.getElementById('video');
     var audio = document.getElementById('audio');
 
-    button.addEventListener('click', function() {
-        // Verifica se o áudio está pausado
-        if (audio.paused) {
-            // Inicia a reprodução do áudio
-            audio.play();
-        } else {
-            // Pausa o áudio se já estiver reproduzindo
-            audio.pause();
-            // Define o tempo de reprodução para 0 para reiniciar o áudio
-            audio.currentTime = 0;
-        }
+    playButton.addEventListener('click', function() {
+        // Oculta o botão quando é clicado
+        playButton.style.display = 'none';
+        
+        // Exibe o vídeo em tela cheia e reproduz
+        video.style.display = 'block';
+        video.play();
+        
+        // Modifica o volume do vídeo para 0.5 (meio volume)
+        video.volume = 0.95;
+        
+        // Reproduz o áudio
+        //audio.play();
+    });
+
+    // Mostra o botão novamente quando o vídeo terminar
+    video.addEventListener('ended', function() {
+        // Exibe o botão
+        playButton.style.display = 'block';
+        // Oculta o vídeo
+        video.style.display = 'none';
+        // Volta o fundo para branco
+        document.body.style.backgroundColor = 'white';
     });
 });
